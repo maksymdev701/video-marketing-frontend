5c0158a6447254d6b5032c93cdc1d685afe3a80f
violetstar701 <violetstar701@gmail.com>
2023-06-25 15:44:22 +0300

    jackpot page

--- src/components/navbar-admin.js
+++ src/components/navbar-admin.js
@@ -8,11 +8,11 @@ import './navbar-admin.css'
 
 const NavbarAdmin = (props) => {
   return (
-    <div className="navbar-admin-container">
+    <div className={`navbar-admin-container ${props.rootClassName} `}>
       <header data-thq="thq-navbar" className="navbar-admin-navbar-interactive">
         <img
           alt={props.image_alt}
-          src={props.image_src}
+          src={props.logo}
           loading="eager"
           className="navbar-admin-image"
         />
@@ -27,23 +27,35 @@ const NavbarAdmin = (props) => {
             className="navbar-admin-nav"
           >
             <Link to="/dashboard" className="navbar-admin-navlink">
-              {props.text}
+              {props.Dashboard}
             </Link>
-            <Link to="/download" className="navbar-admin-navlink1">
-              {props.text1}
+            <Link to="/jackpot" className="navbar-admin-navlink1">
+              {props.Jackpot}
             </Link>
-            <Link to="/account" className="navbar-admin-navlink2">
-              {props.text2}
+            <Link to="/users" className="navbar-admin-navlink2">
+              {props.Users}
+            </Link>
+            <Link to="/download" className="navbar-admin-navlink3">
+              {props.Download}
+            </Link>
+            <Link to="/account" className="navbar-admin-navlink4">
+              {props.Account}
             </Link>
           </nav>
-          <Language rootClassName="language-root-class-name3"></Language>
+          <Language
+            rootClassName="language-root-class-name3"
+            className=""
+          ></Language>
           <button type="button" className="navbar-admin-button button">
-            {props.button}
+            {props.logout}
           </button>
         </div>
         <div data-thq="thq-burger-menu" className="navbar-admin-burger-menu">
           <svg viewBox="0 0 1024 1024" className="navbar-admin-icon">
-            <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
+            <path
+              d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"
+              className=""
+            ></path>
           </svg>
         </div>
         <div data-thq="thq-mobile-menu" className="navbar-admin-mobile-menu">
@@ -54,8 +66,8 @@ const NavbarAdmin = (props) => {
           >
             <div className="navbar-admin-container1">
               <img
-                alt={props.image_alt1}
-                src={props.image_src1}
+                alt="image"
+                src="https://presentation-website-assets.teleporthq.io/logos/logo.png"
                 className="navbar-admin-image1"
               />
               <div
@@ -63,7 +75,10 @@ const NavbarAdmin = (props) => {
                 className="navbar-admin-menu-close"
               >
                 <svg viewBox="0 0 1024 1024" className="navbar-admin-icon02">
-                  <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+                  <path
+                    d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"
+                    className=""
+                  ></path>
                 </svg>
               </div>
             </div>
@@ -72,17 +87,19 @@ const NavbarAdmin = (props) => {
               data-role="Nav"
               className="navbar-admin-nav2"
             >
-              <span className="navbar-admin-text">{props.text4}</span>
-              <span className="navbar-admin-text1">{props.text5}</span>
-              <span className="navbar-admin-text2">{props.text6}</span>
-              <span className="navbar-admin-text3">{props.text7}</span>
-              <span className="navbar-admin-text4">{props.text8}</span>
+              <span className="navbar-admin-text">About</span>
+              <span className="navbar-admin-text1">Features</span>
+              <span className="navbar-admin-text2">Pricing</span>
+              <span className="navbar-admin-text3">Pricing</span>
+              <span className="navbar-admin-text4">Pricing</span>
+              <span className="navbar-admin-text5">Team</span>
+              <span className="navbar-admin-text6">Blog</span>
             </nav>
             <div className="navbar-admin-container2">
               <button className="navbar-admin-login button">
                 {props.Login}
               </button>
-              <button className="button">{props.Register}</button>
+              <button className="button">Register</button>
             </div>
           </div>
           <div className="navbar-admin-icon-group">
@@ -90,19 +107,28 @@ const NavbarAdmin = (props) => {
               viewBox="0 0 950.8571428571428 1024"
               className="navbar-admin-icon04"
             >
-              <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+              <path
+                d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"
+                className=""
+              ></path>
             </svg>
             <svg
               viewBox="0 0 877.7142857142857 1024"
               className="navbar-admin-icon06"
             >
-              <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+              <path
+                d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"
+                className=""
+              ></path>
             </svg>
             <svg
               viewBox="0 0 602.2582857142856 1024"
               className="navbar-admin-icon08"
             >
-              <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+              <path
+                d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"
+                className=""
+              ></path>
             </svg>
           </div>
         </div>
@@ -112,40 +138,29 @@ const NavbarAdmin = (props) => {
 }
 
 NavbarAdmin.defaultProps = {
-  image_alt1: 'image',
   image_alt: 'logo',
-  image_src1:
-    'https://presentation-website-assets.teleporthq.io/logos/logo.png',
-  text2: 'Account',
-  text8: 'Blog',
-  text5: 'Features',
-  button: 'Log Out',
-  text6: 'Pricing',
-  text1: 'Download',
+  Account: 'Account',
+  rootClassName: '',
+  logout: 'Log Out',
+  Jackpot: 'Jackpot',
+  Download: 'Download',
   Login: 'Login',
-  text: 'Dashboard',
-  image_src: '/eurasia%20media%20logo.svg',
-  text4: 'About',
-  Register: 'Register',
-  text7: 'Team',
+  Dashboard: 'Dashboard',
+  logo: '/eurasia-logo-r.svg',
+  Users: 'Users',
 }
 
 NavbarAdmin.propTypes = {
-  image_alt1: PropTypes.string,
   image_alt: PropTypes.string,
-  image_src1: PropTypes.string,
-  text2: PropTypes.string,
-  text8: PropTypes.string,
-  text5: PropTypes.string,
-  button: PropTypes.string,
-  text6: PropTypes.string,
-  text1: PropTypes.string,
+  Account: PropTypes.string,
+  rootClassName: PropTypes.string,
+  logout: PropTypes.string,
+  Jackpot: PropTypes.string,
+  Download: PropTypes.string,
   Login: PropTypes.string,
-  text: PropTypes.string,
-  image_src: PropTypes.string,
-  text4: PropTypes.string,
-  Register: PropTypes.string,
-  text7: PropTypes.string,
+  Dashboard: PropTypes.string,
+  logo: PropTypes.string,
+  Users: PropTypes.string,
 }
 
 export default NavbarAdmin
--- src/views/jackpot.js
+++ src/views/jackpot.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import { toast } from "react-toastify";
 
-import { Helmet } from 'react-helmet'
+import NavbarAdmin from "../components/navbar-admin";
+import Footer from "../components/footer";
+import {
+  useUpdateJackpotMutation,
+  useGetCreatorStatsQuery,
+} from "../redux/api/stats-api";
+import FullScreenLoader from "../components/fullscreen-loader";
 
-import NavbarAdmin from '../components/navbar-admin'
-import Footer from '../components/footer'
-import './jackpot.css'
+import "./jackpot.css";
 
 const Jackpot = (props) => {
+  const [jackpot, setJackpot] = useState(0);
+  const [editable, setEditable] = useState(false);
+  const [updateJackpot, updateStates] = useUpdateJackpotMutation();
+  const { isLoading, data, refetch } = useGetCreatorStatsQuery();
+
+  useEffect(() => {
+    if (updateStates.isSuccess) {
+      toast.success("Jackpot updated successfully!");
+      refetch();
+    }
+  }, [updateStates.isLoading]);
+
+  if (isLoading) return <FullScreenLoader />;
+
   return (
     <div className="jackpot-container">
       <Helmet>
@@ -45,7 +64,9 @@ const Jackpot = (props) => {
                   />
                 </div>
               </div>
-              <h1 className="jackpot-text04 StatsBig">€12,000</h1>
+              <h1 className="jackpot-text04 StatsBig">
+                €{data.jackpot.toLocaleString()}
+              </h1>
               <span className="jackpot-text05 StatTitle">Jackpot</span>
               <span className="jackpot-text06">Total cash pool this month</span>
             </div>
@@ -55,7 +76,9 @@ const Jackpot = (props) => {
                   <path d="M832 192v-128h-640v128h-192v128c0 106.038 85.958 192 192 192 20.076 0 39.43-3.086 57.62-8.802 46.174 66.008 116.608 113.796 198.38 130.396v198.406h-64c-70.694 0-128 57.306-128 128h512c0-70.694-57.306-128-128-128h-64v-198.406c81.772-16.6 152.206-64.386 198.38-130.396 18.19 5.716 37.544 8.802 57.62 8.802 106.042 0 192-85.962 192-192v-128h-192zM192 436c-63.962 0-116-52.038-116-116v-64h116v64c0 40.186 7.43 78.632 20.954 114.068-6.802 1.246-13.798 1.932-20.954 1.932zM948 320c0 63.962-52.038 116-116 116-7.156 0-14.152-0.686-20.954-1.932 13.524-35.436 20.954-73.882 20.954-114.068v-64h116v64z"></path>
                 </svg>
               </div>
-              <h1 className="jackpot-text07 StatsBig">€3,000</h1>
+              <h1 className="jackpot-text07 StatsBig">
+                €{(data.jackpot / 4).toLocaleString()}
+              </h1>
               <span className="jackpot-text08 StatTitle">Champions Bonus</span>
               <span className="jackpot-text09">Champions Bonus this month</span>
             </div>
@@ -63,7 +86,9 @@ const Jackpot = (props) => {
               <svg viewBox="0 0 1024 1024" className="jackpot-icon2">
                 <path d="M598 554h128l-214-212-214 212h128v172h172v-172zM826 428q82 6 140 67t58 145q0 88-63 151t-151 63h-554q-106 0-181-75t-75-181q0-94 67-169t161-85q42-78 118-126t166-48q108 0 201 76t113 182z"></path>
               </svg>
-              <h1 className="jackpot-text10 StatsBig">10,000</h1>
+              <h1 className="jackpot-text10 StatsBig">
+                {data.total_uploads.toLocaleString()}
+              </h1>
               <span className="jackpot-text11 StatTitle">Videos Uploaded</span>
               <span className="jackpot-text12">
                 Videos uploaded by all creators
@@ -78,7 +103,9 @@ const Jackpot = (props) => {
                   <path d="M922.857 0c52 0 100 38.857 100 93.143 0 30.286-12 59.429-25.714 86.286-44.571 84.571-194.286 364-265.714 429.714-34.857 32.571-76 52-124.571 52-96.571 0-175.429-82.286-175.429-178.286 0-45.714 18.857-90.286 52.571-121.143l364.571-330.857c20-18.286 46.286-30.857 74.286-30.857zM403.429 590.857c29.714 57.714 84 101.143 146.857 117.714l0.571 40.571c3.429 162.857-109.714 274.857-273.143 274.857-193.714 0-277.714-154.286-277.714-329.714 21.143 14.286 94.857 73.143 118.857 73.143 14.286 0 26.286-8 31.429-21.143 48.571-126.857 124.571-149.714 253.143-155.429z"></path>
                 </svg>
               </div>
-              <h1 className="jackpot-text13 StatsBig">12,000</h1>
+              <h1 className="jackpot-text13 StatsBig">
+                {data.creators.toLocaleString()}
+              </h1>
               <span className="jackpot-text14 StatTitle">Creators</span>
               <span className="jackpot-text15">Number of creators</span>
             </div>
@@ -104,76 +131,24 @@ const Jackpot = (props) => {
           </div>
         </div>
         <div className="jackpot-table-body">
-          <div className="jackpot-table-row">
-            <div className="jackpot-container10">
-              <span className="jackpot-text20">1</span>
+          {data.champions.map((champion, index) => (
+            <div className="jackpot-table-row" key={index}>
+              <div className="jackpot-container10">
+                <span className="jackpot-text20">{index + 1}</span>
+              </div>
+              <div className="jackpot-container11">
+                <span className="jackpot-text21">{champion.creator}</span>
+              </div>
+              <div className="jackpot-container12">
+                <span className="jackpot-text22">€5,000,000</span>
+              </div>
+              <div className="jackpot-container13">
+                <span className="jackpot-text23">
+                  {champion.uploads.toLocaleString()}
+                </span>
+              </div>
             </div>
-            <div className="jackpot-container11">
-              <span className="jackpot-text21">Leo Messi</span>
-            </div>
-            <div className="jackpot-container12">
-              <span className="jackpot-text22">€5,000,000</span>
-            </div>
-            <div className="jackpot-container13">
-              <span className="jackpot-text23">2,000,000</span>
-            </div>
-          </div>
-          <div className="jackpot-table-row1">
-            <div className="jackpot-container14">
-              <span className="jackpot-text24">2</span>
-            </div>
-            <div className="jackpot-container15">
-              <span className="jackpot-text25">Leo Messi</span>
-            </div>
-            <div className="jackpot-container16">
-              <span className="jackpot-text26">€5,000,000</span>
-            </div>
-            <div className="jackpot-container17">
-              <span className="jackpot-text27">2,000,000</span>
-            </div>
-          </div>
-          <div className="jackpot-table-row2">
-            <div className="jackpot-container18">
-              <span className="jackpot-text28">3</span>
-            </div>
-            <div className="jackpot-container19">
-              <span className="jackpot-text29">Leo Messi</span>
-            </div>
-            <div className="jackpot-container20">
-              <span className="jackpot-text30">€5,000,000</span>
-            </div>
-            <div className="jackpot-container21">
-              <span className="jackpot-text31">2,000,000</span>
-            </div>
-          </div>
-          <div className="jackpot-table-row3">
-            <div className="jackpot-container22">
-              <span className="jackpot-text32">4</span>
-            </div>
-            <div className="jackpot-container23">
-              <span className="jackpot-text33">Leo Messi</span>
-            </div>
-            <div className="jackpot-container24">
-              <span className="jackpot-text34">€5,000,000</span>
-            </div>
-            <div className="jackpot-container25">
-              <span className="jackpot-text35">2,000,000</span>
-            </div>
-          </div>
-          <div className="jackpot-table-row4">
-            <div className="jackpot-container26">
-              <span className="jackpot-text36">5</span>
-            </div>
-            <div className="jackpot-container27">
-              <span className="jackpot-text37">Leo Messi</span>
-            </div>
-            <div className="jackpot-container28">
-              <span className="jackpot-text38">€5,000,000</span>
-            </div>
-            <div className="jackpot-container29">
-              <span className="jackpot-text39">2,000,000</span>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <div className="jackpot-manual-inputs">
@@ -183,57 +158,35 @@ const Jackpot = (props) => {
           </span>
           <input
             type="text"
-            placeholder="placeholder"
+            placeholder="Jackpot Value"
             className="jackpot-textinput input"
+            disabled={!editable}
+            defaultValue={data.jackpot}
+            onChange={(e) => setJackpot(e.target.value)}
           />
-          <button type="button" className="jackpot-button button">
+          <button
+            type="button"
+            className="jackpot-button button"
+            disabled={editable}
+            onClick={() => setEditable(true)}
+          >
             EDIT
           </button>
-          <Link to="/new-user-pop-up" className="jackpot-navlink button">
+          <button
+            className="jackpot-navlink button"
+            disabled={!editable}
+            onClick={() => {
+              updateJackpot({ jackpot });
+              setEditable(false);
+            }}
+          >
             CONFIRM
-          </Link>
-        </div>
-        <div className="jackpot-all-time-revenue">
-          <span className="jackpot-text41">
-            Input the revenue for this month
-          </span>
-          <input
-            type="text"
-            placeholder="placeholder"
-            className="jackpot-textinput1 input"
-          />
-          <button type="button" className="jackpot-button1 button">
-            EDIT
           </button>
-          <Link to="/new-user-pop-up" className="jackpot-navlink1 button">
-            CONFIRM
-          </Link>
-        </div>
-        <div className="jackpot-jackpot-setup">
-          <span className="jackpot-text42">
-            Input the all-time revenue
-            <span
-              dangerouslySetInnerHTML={{
-                __html: ' ',
-              }}
-            />
-          </span>
-          <input
-            type="text"
-            placeholder="placeholder"
-            className="jackpot-textinput2 input"
-          />
-          <button type="button" className="jackpot-button2 button">
-            EDIT
-          </button>
-          <Link to="/new-user-pop-up" className="jackpot-navlink2 button">
-            CONFIRM
-          </Link>
         </div>
       </div>
       <Footer rootClassName="footer-root-class-name7"></Footer>
     </div>
-  )
-}
+  );
+};
 
-export default Jackpot
+export default Jackpot;
